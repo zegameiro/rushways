9983f99b61a8495219a2d56a17e806c9f692c2dd
JoaoCLS <joao.carlos.santos@ua.pt>
2023-05-05 19:29:53 +0100

    Home page (quase acabada)

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import OcasionalServices from './pages/OcasionalServices';
+import Homepage from './Homepage';
 
 const App = () => {
 
     return (
         <div className="App">
-            <OcasionalServices />
+            <Homepage />
         </div>
     );
 }
--- src/Homepage.js
+++ src/Homepage.js
@@ -6,20 +6,48 @@ import Footer from './Components/Footer';
 import './Homepage.css';
 
 const Homepage = () => {
+
+    const vanImage = require('./images/Homepage/van.png')
+    const carImage = require('./images/Homepage/car fleet.png')
     
     return (
         <>
             <Navbar />
-            <div className="homepage">
-                <h1>Get Moving with us</h1>
-                <h4 className="one">Join the RushWays community and stop<p></p>
-                    worring about your transportation needs
-                </h4>
-                <button className="ChooseService"><h5>Choose Service</h5></button>
-                <h4 className="two">Choose what type of service fits you better</h4>
-                
+            <div style={{height:5000}}>
+                <div className="first_element">
+                    <h1>Get Moving with us</h1>
+                    <h4 className="one">Join the RushWays community and stop<p></p>
+                        worring about your transportation needs
+                    </h4>
+                    <button className="ChooseService"><h5>Choose Service</h5></button>
+                    <h4 className="two">Choose what type of service fits you better</h4>
+                </div>
+                <div className="second_element">
+                    <h1 className="phrase1">Why our costumers <p>choose <span className="RushWays">RushWays</span></p></h1>
+                    <h2 className="phrase2">
+                        <pre>“Since the beginning of our contract<br></br>
+                        with RushWays, they have always<br></br>
+                        been very helpful and we are<br></br>
+                        looking forward to keep their<br></br>
+                        service for a long time”</pre>
+                    </h2>
+                    <h3 className="phrase3">Escola profissional de Aveiro is with us since 2020</h3>
+                </div>
+                <div className="third_element">
+                    <img src={vanImage} alt="van" className="image1"></img>
+                </div>
+                <div className="container">
+                    <div className="text">
+                        <h1 className="phrase4">Vehicles in<br></br><span className="RushWays">RushWays</span></h1>
+                        <h4 className="phrase5">Find more information about the vehicles that <br></br> are available to you and explore various type<br></br> of services</h4>
+                    </div>
+                    <div>
+                        <img src={carImage} alt="fleet" className="image2"></img>
+                    </div>
+                    
+                </div>
             </div>
-            <Footer />
+            
         </>
     );
 
