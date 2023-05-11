9e9fd785c0bddb1dd1a0d951b47a3b1d6e1dd088
Ricardo Quintaneiro <97852246+ricardoquintaneiro@users.noreply.github.com>
2023-05-11 03:20:34 +0100

    Modal message change

--- src/Components/ThankYouScreen/index.js
+++ src/Components/ThankYouScreen/index.js
@@ -2,16 +2,18 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
+
 const ThankYouScreen = () => {
 
     return (
         <div className='thank-you-screen'>
             <p className='thank-you-text'>
                 Thank you for you're selection!
-                Our company will enter in contact with you during the next 2-3 days to discuss the price and additional information
+                Our company will enter in contact with you during the next 2-3 days to discuss the price and additional information.<br/>
+                Get your sofas ready. We're coming!
             </p>
             <a href='/'>
-                <button type="button" class="btn btn-primary return-homepage">Return to Homepage</button>
+                <button type="button" className="btn btn-primary return-homepage">Return to Homepage</button>
             </a>
         </div>
     );
